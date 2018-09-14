8b228c585c80d5575b05be0c15846649e85ef7a0
ritc9446 <42875633+ritc9446@users.noreply.github.com>
2018-09-14 14:56:25 -0600

    Project 08

---
+++ project/08/add_form.php
@@ -0,0 +1,10 @@
+<h2>Add Form</h2>
+<form action="add.php" method="get">
+        <label>What is your name?</label>
+        <input type="text" name="my_name">
+        
+        <label>What is your email?</label>
+        <input type="text" name="my_email">
+        
+        <input type="submit" value="Save"/>
+</form>
